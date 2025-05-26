418f6ef4fa6753a8a3e9ddd7aad99cf61c1977cd
mamun2519 <juborajislam46@gmail.com>
2025-05-26 14:48:20 +0600

    feat: added to web scraping

--- basic_puppeteer/web_scraping.js
+++ basic_puppeteer/web_scraping.js
@@ -1,5 +1,19 @@
 import puppeteer from "puppeteer";
 
-const run = async () => {};
+const run = async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://youtube.com");
+
+    // get a page title
+    const title = await page.title();
+
+    console.log("Page Title:", title);
+    // get the headings on the page
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 await run();
