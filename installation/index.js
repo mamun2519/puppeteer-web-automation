0c83951a155241b9b51f375521f2d06965e958bb
mamun2519 <juborajislam46@gmail.com>
2025-05-26 14:28:25 +0600

    feat: install to puppeteer and take a scheenshot

--- installation/index.js
+++ installation/index.js
@@ -1,18 +1,22 @@
 const puppeteer = require("puppeteer");
 
 const puppeteerServer = async () => {
-  // launce a headless browser
-  const browser = await puppeteer.launch({ headless: true });
+  try {
+    // launce a headless browser
+    const browser = await puppeteer.launch({ headless: true });
 
-  // create a new page
-  const page = await browser.newPage();
+    // create a new page
+    const page = await browser.newPage();
 
-  // navigate to a url
-  await page.goto("https://google.com");
-  // take a screenshot
-  await page.screenshot({ path: "google.png" });
-  // close the browser
-  await browser.close();
+    // navigate to a url
+    await page.goto("https://google.com");
+    // take a screenshot
+    await page.screenshot({ path: "google.png" });
+    // close the browser
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await puppeteerServer();
