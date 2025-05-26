8ee502ceafb0eccf1092de49673827489c887bf3
mamun2519 <juborajislam46@gmail.com>
2025-05-26 15:56:14 +0600

    feat: extract web page data

--- basic_puppeteer/src/extract_web_data.js
+++ basic_puppeteer/src/extract_web_data.js
@@ -3,6 +3,17 @@ import puppeteer from "puppeteer";
 const run = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+
+  // navigate to the page
+  await page.goto("https://google.com");
+
+  // extract the page image
+  const image = await page.$$eval("img", (images) =>
+    images.map((img) => {
+      src: img.src;
+      alt: img.alt;
+    })
+  );
 };
 
 await run();
