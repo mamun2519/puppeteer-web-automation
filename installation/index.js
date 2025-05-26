979b03d6a7dfd272551236059b753ea7e976f40f
mamun2519 <juborajislam46@gmail.com>
2025-05-26 14:25:56 +0600

    feat: install to puppeteer and take a scheenshot

--- installation/index.js
+++ installation/index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const puppeteer = require("puppeteer");
+
+const puppeteerServer = async () => {
+  // launce a headless browser
+  const browser = await puppeteer.launch({ headless: true });
+
+  // create a new page
+  const page = await browser.newPage();
+};
+
+await puppeteerServer();
