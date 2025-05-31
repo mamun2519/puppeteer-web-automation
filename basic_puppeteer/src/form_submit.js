607caab85f6e1cc50fd739be6a34901e132f4f2a
mamun2519 <juborajislam46@gmail.com>
2025-05-31 11:46:53 +0600

    feat: added to automate from submation

--- basic_puppeteer/src/form_submit.js
+++ basic_puppeteer/src/form_submit.js
@@ -9,4 +9,8 @@ const enterFormData = async (URL, searchQuery) => {
   await page.focus("input[name='q']"); // Focus on the search input field
   await page.keyboard.type(searchQuery); // Type the search query
   await page.keyboard.press("Enter"); // Press Enter to submit the form
+  await page.waitForNavigation({ waitUntil: "networkidle2" }); // Wait for the navigation to complete
+
+  await page.screenshot({ page: "search_result.png" }); // Take a screenshot of the search results page
+  await browser.close();
 };
