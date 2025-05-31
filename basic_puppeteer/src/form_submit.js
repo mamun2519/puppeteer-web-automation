422a56a87617723867a35be22554a4e2b67a7bd9
mamun2519 <juborajislam46@gmail.com>
2025-05-31 11:50:04 +0600

    feat: added to exract website source code using puppteer

--- basic_puppeteer/src/form_submit.js
+++ basic_puppeteer/src/form_submit.js
@@ -6,7 +6,7 @@ const enterFormData = async (URL, searchQuery) => {
   const page = await browser.newPage();
   await page.goto(URL);
 
-  await page.focus("input[name='q']"); // Focus on the search input field
+  await page.focus("textarea[name='q']"); // Focus on the search input field
   await page.keyboard.type(searchQuery); // Type the search query
   await page.keyboard.press("Enter"); // Press Enter to submit the form
   await page.waitForNavigation({ waitUntil: "networkidle2" }); // Wait for the navigation to complete
