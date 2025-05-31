08a0099bd1aee22a1150fba58cef41741cd46088
mamun2519 <juborajislam46@gmail.com>
2025-05-31 11:27:47 +0600

    feat: added to exract website source code using puppteer

--- basic_puppeteer/src/source_code_extract.js
+++ basic_puppeteer/src/source_code_extract.js
@@ -1,2 +1,11 @@
 // Extract Website Source Code using Puppeteer
 import puppeteer from "puppeteer";
+import fs from "fs";
+
+const getSourceCode = async (url) => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  // get the page source code
+};
