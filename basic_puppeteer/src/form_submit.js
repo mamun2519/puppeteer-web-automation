8bad5507da18ae66a13e652a1563876046440dda
mamun2519 <juborajislam46@gmail.com>
2025-05-31 11:45:52 +0600

    feat: added to automate from submation

--- basic_puppeteer/src/form_submit.js
+++ basic_puppeteer/src/form_submit.js
@@ -1,2 +1,12 @@
 // form data submission using puppeteer
 import puppeteer from "puppeteer";
+
+const enterFormData = async (URL, searchQuery) => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(URL);
+
+  await page.focus("input[name='q']"); // Focus on the search input field
+  await page.keyboard.type(searchQuery); // Type the search query
+  await page.keyboard.press("Enter"); // Press Enter to submit the form
+};
