6d4d6a8ebf9140bb4356624cb7692203829f3751
mamun2519 <juborajislam46@gmail.com>
2025-06-01 11:49:38 +0600

    feat: added to code coverage

---
+++ basic_puppeteer/src/code_coverage.js
@@ -0,0 +1,7 @@
+import puppeteer from "puppeteer";
+
+const codeCoverage = async (URL) => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  // Enable code coverage
+};
