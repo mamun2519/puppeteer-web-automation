3b85dec6caf8ac79722b4be51bd673991a779daf
mamun2519 <juborajislam46@gmail.com>
2025-06-01 12:04:29 +0600

    feat: added to high_light links

--- basic_puppeteer/src/highLight_links.js
+++ basic_puppeteer/src/highLight_links.js
@@ -1,13 +1,15 @@
 import puppeteer from "puppeteer";
 
-const codeCoverage = async (URL) => {
+const highLightLinks = async (URL) => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+
+  await page.goto(URL);
 
   await browser.close();
 };
 
 // Example usage
-codeCoverage("https://google.com")
+highLightLinks("https://google.com")
   .then(() => console.log("Code coverage completed"))
   .catch((error) => console.error("Error during code coverage:", error));
