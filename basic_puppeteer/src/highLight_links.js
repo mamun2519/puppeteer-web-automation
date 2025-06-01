d8e57f0aa98c3e397e9baa7140fca521b24b6b5e
mamun2519 <juborajislam46@gmail.com>
2025-06-01 12:03:45 +0600

    feat: added to high_light links

---
+++ basic_puppeteer/src/highLight_links.js
@@ -0,0 +1,30 @@
+import puppeteer from "puppeteer";
+
+const codeCoverage = async (URL) => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  // Enable code coverage
+  await Promise.all([
+    page.coverage.startJSCoverage(),
+    page.coverage.startCSSCoverage(),
+  ]);
+
+  // Navigate to the URL
+  await page.goto(URL);
+
+  // Stop code coverage
+  const [jsCoverage, cssCoverage] = await Promise.all([
+    page.coverage.stopJSCoverage(),
+    page.coverage.stopCSSCoverage(),
+  ]);
+  // log the coverage data
+  console.log("JavaScript Coverage:", jsCoverage);
+  console.log("CSS Coverage:", cssCoverage);
+
+  await browser.close();
+};
+
+// Example usage
+codeCoverage("https://google.com")
+  .then(() => console.log("Code coverage completed"))
+  .catch((error) => console.error("Error during code coverage:", error));
