6f0465fb90062831ce54b58ef41e315964f1d13d
mamun2519 <juborajislam46@gmail.com>
2025-06-01 12:05:44 +0600

    feat: added to high_light links

--- basic_puppeteer/src/highLight_links.js
+++ basic_puppeteer/src/highLight_links.js
@@ -6,6 +6,16 @@ const highLightLinks = async (URL) => {
 
   await page.goto(URL);
 
+  await page.screenshot({ path: "before_highlight.png" });
+
+  await page.evaluate(() => {
+    const links = document.querySelectorAll("a");
+
+    links.forEach((link) => {
+      link.style.backgroundColor = "yellow"; // Highlight links with yellow background
+      link.style.color = "red"; // Change link text color to red
+    });
+  });
   await browser.close();
 };
 
